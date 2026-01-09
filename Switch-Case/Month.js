8aee11fc9f068a85552ba8c3a0dbd6d941385d18
Sunag Arigala <ramsunag@gmail.com>
2026-01-09 11:27:03 +0530

    switch case

---
+++ Switch-Case/Month.js
@@ -0,0 +1,42 @@
+let month = 1;
+
+switch(month){
+  case 1:
+    console.log("31 Days january");
+    break;
+  case 2:
+    console.log("28 Days february");
+    break;
+  case 3:
+    console.log("31 Days march");
+    break;
+  case 4:
+    console.log("30 Days april");
+    break;
+  case 5:
+    console.log("31 Days may");
+    break;
+  case 6:
+    console.log("30 Days june ");
+    break;
+  case 7:
+    console.log("31 Days july");
+    break; 
+  case 8:
+    console.log("31 Days august");
+    break; 
+  case 9:
+    console.log("30 Days september");
+    break;
+  case 10:
+    console.log("31 Days october");
+    break;
+  case 11:
+    console.log("30 Days november");
+    break; 
+  case 12:
+    console.log("31 Days december");
+    break;
+  default:
+    console.log("select month in between 1-12")                          
+}
---
+++ Switch-Case/VowelsOrconsonents.js
@@ -0,0 +1,22 @@
+let letter = "d";
+
+switch(letter){
+    case "a":
+        console.log(`${letter} vowel`);
+        break;
+    case "e":
+    console.log(`${letter} vowel`);
+          break;
+    case "i":
+    console.log(`${letter} vowel`);
+          break;
+    case "o":
+    console.log(`${letter} vowel`);
+          break;
+    case "u":
+    console.log(`${letter} vowel`);
+          break;
+    default:
+        console.log(`${letter} consonent`);
+
+}
